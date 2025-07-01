72ffa29db07b5899c4e7f962d8dc7edefdb767d3
GrzegorzGruca <grzegorzgruca7714@gmail.com>
2025-07-01 16:14:35 +0200

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: '/SSOnameGenerator/', // WAŻNE: Zastąp 'NAZWA_TWOJEGO_REPOZYTORIUM' rzeczywistą nazwą swojego repozytorium GitHub
 
 })
